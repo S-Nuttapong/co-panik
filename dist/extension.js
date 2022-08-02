42c32397e104864b0fb693ef173c3ac61cf10b4c
Nuttapong Saelek <nuttapong_s@kkumail.com>
2022-08-02 15:55:30 +0700

    feat: init text decoration

--- dist/extension.js
+++ dist/extension.js
@@ -34,6 +34,40 @@ exports.deactivate = exports.activate = exports.withEnableCommand = void 0;
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(__webpack_require__(1));
 const vscode_1 = __webpack_require__(1);
+let posX = 0;
+const objectToCssString = (settings) => {
+    let value = '';
+    const cssString = Object.keys(settings).map(setting => {
+        value = settings[setting];
+        if (typeof value === 'string' || typeof value === 'number') {
+            return `${setting}: ${value};`;
+        }
+    }).join(' ');
+    return cssString;
+};
+const DEFAULT_CSS = objectToCssString({
+    position: 'absolute',
+    right: "5%",
+    top: "20px",
+    ['font-family']: "monospace",
+    ['font-weight']: "900",
+    // width: "50px",
+    ['z-index']: 1,
+    ['pointer-events']: 'none',
+    ["text-align"]: "right",
+});
+const imgUrl = "https://raw.githubusercontent.com/ao-shen/vscode-power-mode/master/images/Character_Diona_Portrait.png";
+const bgImgCss = objectToCssString({
+    width: `60vh`,
+    height: `80vh`,
+    "background-repeat": 'no-repeat',
+    ["background-size"]: 'contain',
+    ["background-position"]: 'right',
+    ["z-index"]: 9999,
+    ["background-color"]: `transparent`,
+    ["background-image"]: `url("${imgUrl}")`,
+    right: `${-posX}vh`,
+});
 function withEnableCommand() {
     return vscode_1.commands.registerCommand("enable", () => {
         vscode_1.window.showInformationMessage("panik enable !!");
@@ -44,9 +78,23 @@ function onDidChangeTextDocument(event) {
     console.debug('text changed', { event });
     console.debug(event.contentChanges);
 }
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
+    console.log({ DEFAULT_CSS, bgImgCss });
+    const editor = vscode.window.activeTextEditor;
+    const decoration = vscode.window.createTextEditorDecorationType({
+        // Title and Count cannot use the same pseudoelement
+        before: {
+            contentText: "",
+            color: "#fff",
+            textDecoration: `none; ${DEFAULT_CSS} ${bgImgCss}`,
+        },
+        rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+    });
+    //@ts-ignore
+    const firstVisibleRange = editor?.visibleRanges.sort()[0];
+    const position = firstVisibleRange.start;
+    const ranges = [new vscode.Range(position, position)];
+    editor?.setDecorations(decoration, ranges);
     vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
 }
 exports.activate = activate;
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,49 @@
 import * as vscode from 'vscode';
 import { commands, window } from "vscode";
 
+let posX = 0;
+
+const objectToCssString = (settings: any) => {
+	let value = '';
+	const cssString = Object.keys(settings).map(setting => {
+		value = settings[setting];
+		if (typeof value === 'string' || typeof value === 'number') {
+			return `${setting}: ${value};`
+		}
+	}).join(' ');
+
+	return cssString;
+}
+
+const DEFAULT_CSS = objectToCssString({
+	position: 'absolute',
+	right: "5%",
+	top: "20px",
+
+	['font-family']: "monospace",
+	['font-weight']: "900",
+
+	// width: "50px",
+	['z-index']: 1,
+	['pointer-events']: 'none',
+	["text-align"]: "right",
+});
+
+const imgUrl = "https://raw.githubusercontent.com/ao-shen/vscode-power-mode/master/images/Character_Diona_Portrait.png"
+
+
+const bgImgCss = objectToCssString({
+	width: `60vh`,
+	height: `80vh`,
+	"background-repeat": 'no-repeat',
+	["background-size"]: 'contain',
+	["background-position"]: 'right',
+	["z-index"]: 9999,
+	["background-color"]: `transparent`,
+	["background-image"]: `url("${imgUrl}")`,
+	right: `${-posX}vh`,
+})
+
 
 export function withEnableCommand() {
 	return commands.registerCommand("enable", () => {
@@ -15,9 +58,29 @@ function onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent) {
 	console.debug(event.contentChanges)
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log({ DEFAULT_CSS, bgImgCss })
+
+	const editor = vscode.window.activeTextEditor
+
+	const decoration = vscode.window.createTextEditorDecorationType({
+		// Title and Count cannot use the same pseudoelement
+		before: {
+			contentText: "",
+			color: "#fff",
+			textDecoration: `none; ${DEFAULT_CSS} ${bgImgCss}`,
+		},
+		rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+	})
+
+	//@ts-ignore
+	const firstVisibleRange = editor?.visibleRanges.sort()[0];
+	const position = firstVisibleRange.start;
+	const ranges = [new vscode.Range(position, position)];
+
+	editor?.setDecorations(decoration, ranges)
+
 	vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument)
 }
 
